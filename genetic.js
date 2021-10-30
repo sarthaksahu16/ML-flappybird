5be4813b47a44200df784f833121f07b56c51dde
bhatiaharshita <93429384+bhatiaharshita@users.noreply.github.com>
2021-10-30 23:59:31 +0530

    Update genetic.js

--- genetic.js
+++ genetic.js
@@ -125,5 +125,37 @@ GeneticAlgorithm.prototype = {
 			return unitA.index - unitB.index;
 		});
 	},
+	// selects the best units from the current population
+	selection : function(){
+		// sort the units of the current population	in descending order by their fitness
+		var sortedPopulation = this.Population.sort(
+			function(unitA, unitB){
+				return unitB.fitness - unitA.fitness;
+			}
+		);
+		
+		// mark the top units as the winners!
+		for (var i=0; i<this.top_units; i++) this.Population[i].isWinner = true;
+		
+		// return an array of the top units from the current population
+		return sortedPopulation.slice(0, this.top_units);
+	},
+	
+	// performs a single point crossover between two parents
+	crossOver : function(parentA, parentB) {
+		// get a cross over cutting point
+		var cutPoint = this.random(0, parentA.neurons.length-1);
+		
+		// swap 'bias' information between both parents:
+		// 1. left side to the crossover point is copied from one parent
+		// 2. right side after the crossover point is copied from the second parent
+		for (var i = cutPoint; i < parentA.neurons.length; i++){
+			var biasFromParentA = parentA.neurons[i]['bias'];
+			parentA.neurons[i]['bias'] = parentB.neurons[i]['bias'];
+			parentB.neurons[i]['bias'] = biasFromParentA;
+		}
+
+		return this.random(0, 1) == 1 ? parentA : parentB;
+	},
 	
 }
