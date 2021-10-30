6a1a34b9f493809fcea4ae8e318ee950574aab54
bhatiaharshita <93429384+bhatiaharshita@users.noreply.github.com>
2021-10-30 23:56:24 +0530

    Update geneticc.js

--- geneticc.js
+++ geneticc.js
@@ -12,3 +12,37 @@ var GeneticAlgorithm = function(max_units, top_units){
 	
 	this.SCALE_FACTOR = 200; // the factor used to scale normalized input values
 }
+
+GeneticAlgorithm.prototype = {
+	// resets genetic algorithm parameters
+	reset : function(){
+		this.iteration = 1;	// current iteration number (it is equal to the current population number)
+		this.mutateRate = 1; // initial mutation rate
+		
+		this.best_population = 0; // the population number of the best unit
+		this.best_fitness = 0;  // the fitness of the best unit
+		this.best_score = 0;	// the score of the best unit ever
+	},
+	
+	// creates a new population
+	createPopulation : function(){
+		// clear any existing population
+		this.Population.splice(0, this.Population.length);
+		
+		for (var i=0; i<this.max_units; i++){
+			// create a new unit by generating a random Synaptic neural network
+			// with 2 neurons in the input layer, 6 neurons in the hidden layer and 1 neuron in the output layer
+			var newUnit = new synaptic.Architect.Perceptron(2, 6, 1);
+			
+			// set additional parameters for the new unit
+			newUnit.index = i;
+			newUnit.fitness = 0;
+			newUnit.score = 0;
+			newUnit.isWinner = false;
+			
+			// add the new unit to the population 
+			this.Population.push(newUnit);
+		}
+	},
+	
+}
