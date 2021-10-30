a065c109936352ad3e9c1418b7da563e1310c6a8
sarthaksahu16 <93446230+sarthaksahu16@users.noreply.github.com>
2021-10-30 23:44:57 +0530

    Create geneticc.js

---
+++ geneticc.js
@@ -0,0 +1,14 @@
+/***********************************************************************************
+/* Genetic Algorithm implementation
+/***********************************************************************************/
+
+var GeneticAlgorithm = function(max_units, top_units){
+	this.max_units = max_units; // max number of units in population
+	this.top_units = top_units; // number of top units (winners) used for evolving population
+	
+	if (this.max_units < this.top_units) this.top_units = this.max_units;
+	
+	this.Population = []; // array of all units in current population
+	
+	this.SCALE_FACTOR = 200; // the factor used to scale normalized input values
+}
